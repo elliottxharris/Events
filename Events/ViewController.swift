0682682c618bb209475103964127dad914cd64f1
elliottxharris <elliottxharris@gmail.com>
2021-06-03 19:23:39 -0500

    Import contacts

--- Events/ViewController.swift
+++ Events/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import ContactsUI
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CNContactPickerDelegate {
+    var contactStore = CNContactStore()
+    var selectedContact: CNContact? = nil
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+        let contactPickerViewController = CNContactPickerViewController()
+        
+        contactPickerViewController.delegate = self
+        
+        present(contactPickerViewController, animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
-
-
+    
+    func showMessge(title: String, message: String) {
+        let alertController = UIAlertController(title: "Contacts", message: message, preferredStyle: UIAlertController.Style.alert)
+        let alertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { action in
+        }
+        
+        alertController.addAction(alertAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+    
+    func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
+        selectedContact = contact
+    }
 }
 
